f28e85f72b116b19dbfc44f8a2d98ffea8bfff99
Ash Luchowa <ashwinluchowa@gmail.com>
2023-11-21 16:17:25 +0800

    Implement 'back-to-top' button

--- js/main.js
+++ js/main.js
@@ -1,51 +1,24 @@
-// Change Text Color
-function scrollNavbar() {
-    var headerSection = document.querySelector('header');
-    var introPosition = headerSection.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight;
-    var mainNav = document.querySelectorAll('#main-nav a');
+//BACK TO TOP SCROLL
+const showOnPx = 100;
+const topBtn = document.querySelector('.back-top');
 
-    if(-introPosition > screenPosition) {
-        for (var j = 0; j < mainNav.length; j++) {
-            mainNav[j].classList.add('text-color-dark');
-        }
+const scrollContainer = () => {
+    return document.documentElement || document.body;
+};
+
+document.addEventListener('scroll', () => {
+    if (scrollContainer().scrollTop > showOnPx) {
+        topBtn.style.opacity = '1';
     } else {
-        for (var y = 0; y < mainNav.length; y++) {
-            mainNav[y].classList.remove('text-color-dark');
-        }
+        topBtn.style.opacity = '0';
     }
-}
+});
 
-// Filter Store Page
-function filterOptions() {
-    var filterItem = document.querySelectorAll('.product-item');
-    var filterBtn = document.querySelectorAll('.filter-btn li a');
-    var eachBtn = document.querySelectorAll('.list-group a');
+//smooth scroll
+const goToTop = () => {
+    document.body.scrollIntoView({
+        behavior: 'smooth',
+    });
+};
 
-    for (var x = 0; x < filterBtn.length; x++ ) {
-        filterBtn[x].addEventListener('click', function(event) {
-            event.preventDefault();
-            var clickedIndex = Array.from(filterBtn).indexOf(this);
-            var singleItem = eachBtn[clickedIndex].textContent;
-
-            // filter each item
-            for (var i = 0; i < filterItem.length; i++) {
-                if(!filterItem[i].className.includes(singleItem)) {
-                    filterItem[i].classList.add('filter-none');
-                } else {
-                    filterItem[i].classList.remove('filter-none');
-                }
-            }
-
-            // show all items
-            if (singleItem === 'All') {
-                for (var a = 0; a < filterItem.length; a++) {
-                    filterItem[a].classList.remove('filter-none');
-                }
-            }
-        });
-    }
-}
-
-filterOptions();
-window.addEventListener('scroll', scrollNavbar);
+topBtn.addEventListener('click', goToTop);
